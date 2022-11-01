28473ab269ecc82cb31ce8022d64dc493066d913
nettenegri <nettenegri@gmail.com>
2022-11-01 09:38:07 -0600

    commit tree 803ff6195dba390729314bd492f115d3632bb0ec

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  // todo
+}
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  // todo
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string) {
+  //todo
+}
+function logShout(string) {
+  return string.toUpperCase();
+}
+console.log(message);
